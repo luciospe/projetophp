ecad6c4bcbdd8c86315957e9ec570abe366c2ebb
luciospe <lucio.home128@gmail.com>
2025-05-09 20:17:24 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="PT-BR">
+<head>
+    <meta charset="YTF-8">
+    <meta  name="viewport"
+     content="widht=device-witdht, inital-scale=1.0">
+
+    <title>TECNOÇOGIAS E SUAS INFORMATICAS</title>
+
+
+    <link rel="stylesheet" 
+    type="text/css" 
+    href="style.css" 
+    media="screean"/>
+   <bods></bods> 
+   
+
+</head>
+
+</html>
+
+
+</html>
